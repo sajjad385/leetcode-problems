35c38a6d1c2705dac5649e1abbfa7c6534094ebe
Mohammad Sajjad Hossain <32493764+sajjad385@users.noreply.github.com>
2022-09-25 15:05:52 +0600

    Update two-sum.php

--- two-sum.php
+++ two-sum.php
@@ -19,3 +19,32 @@
         }
 
     twoSum([2,7,11,15], 9);
+
+
+    /*sample 31 ms submission
+    function twoSum($nums, $target) {
+            $a = [];
+            $size = count($nums);
+            for ($i = 0; $i<$size; $i++) {
+                $diff = $target - $nums[$i];
+                if ( array_key_exists($diff, $a)) {
+                    return [$a[$diff], $i];
+                    break;
+                }
+                $a[$nums[$i]] = $i;
+            }
+        }
+        
+//        sample 17 ms submission 
+        function twoSum($nums, $target) {
+        $map = [];
+        for($i=0; $i<count($nums); $i++) {
+            $diff = $target - $nums[$i];
+            if (array_key_exists($diff, $map)) {
+                return [$map[$diff], $i];
+            } else {
+                $map[$nums[$i]] = $i;
+            }
+        }        
+    }
+    */
